5ed4f56d78f119053361af0ac658647916310b28
yulyazurdia8 <yulyazurdia8@hotmail.com>
2018-09-04 12:23:50 -0600

    Added list for playlist and layouts.

--- app/src/main/java/com/example/dell/lab0_musicstreaming/MainActivity.java
+++ app/src/main/java/com/example/dell/lab0_musicstreaming/MainActivity.java
@@ -2,18 +2,42 @@ package com.example.dell.lab0_musicstreaming;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mySongs;
+    private PlaylistProperties playList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mySongs = (ListView) findViewById(R.id.mySongs);
+        playList = new PlaylistProperties(this, getItems());
+        mySongs.setAdapter(playList);
+    }
+    private ArrayList<Song> getItems()
+    {
+        ArrayList<Song> list = new ArrayList<>();
+
+        list.add(new Song(R.drawable.music, "Prueba", "3:04"));
+        list.add(new Song(R.drawable.music, "Prueba2", "4:00"));
+        list.add(new Song(R.drawable.music, "ysong", "4:00"));
+        list.add(new Song(R.drawable.music, "son2", "4:03"));
+        list.add(new Song(R.drawable.music, "song3", "4:00"));
+        list.add(new Song(R.drawable.music, "song4", "4:00"));
+        list.add(new Song(R.drawable.music, "son5", "4:00"));
+        list.add(new Song(R.drawable.music, "song6", "4:00"));
+        list.add(new Song(R.drawable.music, "song7", "4:00"));
+        list.add(new Song(R.drawable.music, "song8", "4:00"));
 
 
 
-
+        return list;
     }
 
 
--- app/src/main/java/com/example/dell/lab0_musicstreaming/PlaylistProperties.java
+++ app/src/main/java/com/example/dell/lab0_musicstreaming/PlaylistProperties.java
@@ -1,5 +1,54 @@
 package com.example.dell.lab0_musicstreaming;
 
-public class PlaylistProperties {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+public class PlaylistProperties extends BaseAdapter{
+
+    private Context context;
+    private ArrayList<Song> Playlist;
+
+    @Override
+    public int getCount() {
+        return Playlist.size();
+    }
+
+    public PlaylistProperties(Context context, ArrayList<Song> playlist) {
+        this.context = context;
+        Playlist = playlist;
+    }
+
+    @Override
+
+    public Object getItem(int position) {
+        return Playlist.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Song Item = (Song) getItem(position);
+
+        convertView = LayoutInflater.from(context).inflate(R.layout.playlist_show,  null);
+        ImageView imgView = (ImageView) convertView.findViewById(R.id.imgView);
+        TextView txtName = (TextView) convertView.findViewById(R.id.txtName);
+        TextView txtDuration = (TextView) convertView.findViewById(R.id.txtDuration);
+
+        imgView.setImageResource(Item.getIcon());
+        txtName.setText(Item.getName());
+        txtDuration.setText(Item.getDuration());
+
+        return convertView;
+    }
 }
