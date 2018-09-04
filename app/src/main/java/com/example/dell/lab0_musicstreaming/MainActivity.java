8f07a1bc9e8f293a1e74cd88637b3c6df065add1
yulyazurdia8 <yulyazurdia8@hotmail.com>
2018-09-04 15:23:43 -0600

    dictionary and search

--- app/src/main/java/com/example/dell/lab0_musicstreaming/MainActivity.java
+++ app/src/main/java/com/example/dell/lab0_musicstreaming/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dell.lab0_musicstreaming;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import java.util.HashMap;
 
 import java.util.ArrayList;
 
@@ -24,20 +25,36 @@ public class MainActivity extends AppCompatActivity {
     {
         ArrayList<Song> list = new ArrayList<>();
 
-        list.add(new Song(R.drawable.music, "Prueba", "3:04"));
-        list.add(new Song(R.drawable.music, "Prueba2", "4:00"));
-        list.add(new Song(R.drawable.music, "ysong", "4:00"));
-        list.add(new Song(R.drawable.music, "son2", "4:03"));
-        list.add(new Song(R.drawable.music, "song3", "4:00"));
-        list.add(new Song(R.drawable.music, "song4", "4:00"));
-        list.add(new Song(R.drawable.music, "son5", "4:00"));
-        list.add(new Song(R.drawable.music, "song6", "4:00"));
-        list.add(new Song(R.drawable.music, "song7", "4:00"));
-        list.add(new Song(R.drawable.music, "song8", "4:00"));
-
-
+        list.add(new Song(R.drawable.music, "Be the One", "3:04"));
+        list.add(new Song(R.drawable.music, "Empires", "4:30"));
+        list.add(new Song(R.drawable.music, "Closer", "5:01"));
+        list.add(new Song(R.drawable.music, "From The Inside Out", "4:03"));
+        list.add(new Song(R.drawable.music, "Your love is a song", "3:51"));
+        list.add(new Song(R.drawable.music, "Back To Life", "2:40"));
+        list.add(new Song(R.drawable.music, "La otra orilla", "1:20"));
+        list.add(new Song(R.drawable.music, "Heartbeats", "5:11"));
+        list.add(new Song(R.drawable.music, "Closer than you know", "3:03"));
+        list.add(new Song(R.drawable.music, "Every Little Thing", "4:50"));
 
         return list;
+    }
+
+    private HashMap<String, Song> searchItem()
+    {
+        HashMap<String, Song> dictionary = new HashMap<>();
+
+        dictionary.put("Be the One", new Song(R.drawable.music, "Be the One", "3:04"));
+        dictionary.put("Empires", new Song(R.drawable.music, "Empires", "4:30"));
+        dictionary.put("Closer", new Song(R.drawable.music, "Closer", "5:01"));
+        dictionary.put("From The Inside Out", new Song(R.drawable.music, "From The Inside Out", "4:03"));
+        dictionary.put("Your love is a song", new Song(R.drawable.music, "Your love is a song", "3:51"));
+        dictionary.put("Back To Life", new Song(R.drawable.music, "Back To Life", "2:40"));
+        dictionary.put("La otra orilla", new Song(R.drawable.music, "La otra orilla", "1:20"));
+        dictionary.put("Heartbeats", new Song(R.drawable.music, "Heartbeats", "5:11"));
+        dictionary.put("Closer than you know", new Song(R.drawable.music, "Closer than you know", "3:03"));
+        dictionary.put("Every Little Thing", new Song(R.drawable.music, "Every Little Thing", "4:50"));
+
+        return dictionary;
     }
 
 
